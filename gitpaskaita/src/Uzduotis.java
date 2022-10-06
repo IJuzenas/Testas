95f70f321abae3c827a600cfb6d72cf077b3459a
IJuzenas <juzenasignas@gmail.com>
2022-10-06 19:58:07 +0300

    bandymas

---
+++ gitpaskaita/src/Uzduotis.java
@@ -0,0 +1,12 @@
+public class Uzduotis {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 8;
+        float rez;
+        int a1 = a+1;
+        int b1 = b-1;
+        rez = a1 *b1;
+        System.out.println(rez);
+
+    }
+}
