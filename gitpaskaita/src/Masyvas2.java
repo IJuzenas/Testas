30297462d0f9524da1afe12080b219bd6fccffbc
IJuzenas <juzenasignas@gmail.com>
2022-10-06 20:27:09 +0300

    MasyvoUzduotis2

---
+++ gitpaskaita/src/Masyvas2.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class Masyvas2 {
+    public static void main(String[] args) {
+        float [] laikas = {6,4,5,4,2,5,5};
+        System.out.println(laikas.length);
+        System.out.println(laikas [0]);
+        System.out.println(laikas[laikas.length-1]);
+        System.out.println(Arrays.toString(laikas));
+    }
+}
